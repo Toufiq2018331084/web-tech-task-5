bd9eecbae301af7204b01ea67916e759a5091ae3
kevo <kevo@pop-os.localdomain>
2023-05-31 17:55:17 +0600

    1:37:50

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Home from './Home.js'
 import Header from './Header.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-     <Header/>
-     <Home/>
-    </div>
+<Router>
+  <Routes>
+    <Route path='/checkout' element={<>
+      <Header/>
+      <h1>something something</h1>
+    </>} />
+
+    <Route path="/" element={<>
+      <Header/>
+      <Home/>
+    </>} />
+  </Routes>
+</Router>
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import "./Product.css"
 
-function Product({title,image,price,rating}) {
+function Product({id,title,image,price,rating}) {
   return (
     <div className='product'>
               <div className="product__info">
-        <p>demo procuct</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>69</strong>
+          <strong>{price}</strong>
         </p>
         <div className='product_rating'>
+          {Array(rating)
+          .fill()
+          .map((_,i)=>(
             <p>*</p>
-            <p>*</p>
-            <p>*</p>
+          ))}
         </div>
     </div>
-    <img src="https://scontent.fdac24-3.fna.fbcdn.net/v/t39.30808-6/347823434_780356190371535_7996042308379445762_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=9267fe&_nc_eui2=AeGSac_6NAJD2ebUzU3ZcvsJtNsZ7C-0nGm02xnsL7ScaR6QpFGkoz5uLsytBiDeiGOn2kitST6gruFbP2Y_fm38&_nc_ohc=82GIDRc9E-0AX8-It4o&_nc_ht=scontent.fdac24-3.fna&oh=00_AfAGpcs0WdyURjJlGemexVafLFABBu2qFwoZVk0xEkU-vQ&oe=6478C7D7" alt=""/>
+    <img src={image} alt=""/>
     <button>add to cart</button>
     </div>
   )
